f676b6091293438a3edde06308c84f0c82a3fb43
Iain Davis <iain_davis@intuit.com>
2020-12-23 16:48:42 -0800

    add null-handling
    current implementation fails on initialization of OS enum for any other
    platform but WIN, because the ALLUSERSPROFILE is absent. To be sure, we
    want to detect that failure on WIN, but it's irrelevant for all other
    platforms. By returning a non-null result, we avoid the NPE that occurs
    when Paths.get() is invoked, allowing the test to proceed on all other
    platforms.
    
    The test still fails on WIN because the expected value is not produced

--- src/main/java/fxlauncher/model/OS.java
+++ src/main/java/fxlauncher/model/OS.java
@@ -8,6 +8,8 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.EnumMap;
 import java.util.Map;
+import java.util.Optional;
+import java.util.function.Supplier;
 import java.util.logging.Logger;
 
 
@@ -41,7 +43,7 @@ public enum OS {
 				break;
 			case "win":
 				userLibPath = home.resolve("AppData").resolve("Local");
-				allUsersPath = Paths.get(System.getenv("ALLUSERSPROFILE"));
+				allUsersPath = Paths.get(Optional.ofNullable(System.getenv("ALLUSERSPROFILE")).orElse(""));
 				break;
 			default:
 				userLibPath = home;
